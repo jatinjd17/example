ddbb875fd03e9868f8319c041e74d4e32f47c404
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2022-06-29 17:42:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-apt install python3-pip
+sudo apt install python3-pip -y
 wget https://chromedriver.storage.googleapis.com/103.0.5060.53/chromedriver_linux64.zip
 sudo apt install unzip
 unzip chromedriver_linux64.zip
@@ -9,6 +9,6 @@ sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
-sudo apt-get install google-chrome-stable
-pip3 install selenium
+sudo apt-get install google-chrome-stable -y
+sudo pip3 install selenium -y
 python3 startselenium.py
