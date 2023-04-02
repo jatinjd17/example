4d42b5c1836757273802739d8efa37bc72c288bc
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-04-02 11:39:25 +0530

    Update cozi.py

--- cozi.py
+++ cozi.py
@@ -12,14 +12,22 @@ options = webdriver.ChromeOptions()
 # userAgent = ua.random
 # print(userAgent)
 #options.add_argument('--window-size=1920,1080')
-options.add_argument('--window-size=1920,1080')
+options.add_argument("--start-maximized")
+options.add_argument("--disable-blink-features=AutomationControlled") 
+
+ 
+# Exclude the collection of enable-automation switches 
+options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+ 
+# Turn-off userAutomationExtension 
+options.add_experimental_option("useAutomationExtension", False) 
+options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36')
+# # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_argument("--log-level=3")
+# options.add_argument(r"--user-data-dir=C:\Users\jatin\AppData\Local\Google\Chrome\User Data") #e.g. C:\Users\You\AppData\Local\Google\Chrome\User Data
+# options.add_argument('profile-directory=Default')
+# options.add_argument('--window-size=1920,1080')
 options.add_argument("--headless")
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-# options.add_argument("--start-fullscreen")
-#options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
 time.sleep(2)
 
 
