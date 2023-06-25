80955e7c4e57465a81331c0f32df5bbfb53a3133
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-06-25 19:26:18 +0530

    Update cutyio.py

--- cutyio.py
+++ cutyio.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 # from random import randrange
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
-from scrapy.selector import Selector
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
