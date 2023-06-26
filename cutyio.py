5e57db43422075245be3b80b2553382111a9388a
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-06-26 19:30:38 +0530

    Update cutyio.py

--- cutyio.py
+++ cutyio.py
@@ -54,7 +54,7 @@ bot = uc.Chrome(options=options)
 bot.get('https://www.google.com/')
 time.sleep(3)
 bot.save_screenshot('001.png')
-bot.get('https://cuty.io/sVnuSzaQ5o8X')
+bot.get('https://cuty.io/I3kl1')
 
 time.sleep(5)
 
