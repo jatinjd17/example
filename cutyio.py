6f475f68d80488652a3b1555d2afe02730f66f2c
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-06-26 17:56:39 +0530

    Update cutyio.py

--- cutyio.py
+++ cutyio.py
@@ -35,8 +35,8 @@ count = 1
     # print((names.get_first_name(gender='male')+'_'+names.get_last_name()).lower()+str(random.randrange(7598,100000))+'@outlook.com')
 
 
-# options = webdriver.ChromeOptions() 
-options = webdriver.FirefoxOptions() 
+options = webdriver.ChromeOptions() 
+# options = webdriver.FirefoxOptions() 
 
 # options.headless = True
 options.add_argument('--headless')
@@ -46,9 +46,10 @@ options.add_argument('--disable-popup-blocking')
 # lastname = (names.get_last_name()).lower()
 # email = firstname+'_'+lastname+str(random.randrange(400,2000))+'@outlook.com'
 # print(email)
-# bot = uc.Chrome(options=options)
+bot = uc.Chrome(options=options)
 # bot = webdriver.Chrome(chrome_options=options)
-bot = webdriver.Firefox(options=options)
+# bot = webdriver.Chrome(options=options)
+# bot = webdriver.Firefox(options=options)
 
 bot.get('https://www.google.com/')
 time.sleep(3)
@@ -261,3 +262,91 @@ print('Done!!!!!!!!!')
 time.sleep(2)
 
 bot.quit()
+# bot.execute_script("arguments[0].scrollIntoView();", element1)
+
+# WebDriverWait(bot,20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Continue')]"))).click()
+# time.sleep(5)
+
+# element2 = WebDriverWait(bot,20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'I am not a robot')]")))
+# time.sleep(5)
+
+# bot.execute_script("arguments[0].scrollIntoView();", element2)
+
+# WebDriverWait(bot,20).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'I am not a robot')]"))).click()
+
+        
+        
+# time.sleep(10000)        
+        
+
+
+
+
+
+
+
+
+
+
+
+# import time, os 
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# # from random import randrange
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver import ActionChains
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import io
+# import base64
+
+
+# print("INIT")
+# options = webdriver.ChromeOptions()
+# # ua = UserAgent()
+# # userAgent = ua.random
+# # print(userAgent)
+# options.add_argument('--window-size=1920,1080')
+# options.add_argument("--disable-blink-features=AutomationControlled") 
+
+ 
+# # Exclude the collection of enable-automation switches 
+# options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+ 
+# # Turn-off userAutomationExtension 
+# options.add_experimental_option("useAutomationExtension", False) 
+# options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36')
+# # # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# # options.add_argument("--log-level=3")
+# # options.add_argument(r"--user-data-dir=C:\Users\jatin\AppData\Local\Google\Chrome\User Data") #e.g. C:\Users\You\AppData\Local\Google\Chrome\User Data
+# # options.add_argument('profile-directory=Default')
+# # options.add_argument('--window-size=1920,1080')
+# options.add_argument("--headless")
+# time.sleep(2)
+
+# def RunScript():
+#     bot = webdriver.Chrome(options=options)
+
+    
+
+#     bot.execute_script("document.body.style.zoom='30%'")
+
+#     bot.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
+    
+#     bot.get('https://cuty.io/I3kl1')
+
+#     time.sleep(10)
+#     bot.save_screenshot('00111.png')
+
+    
+
+
+
+#     time.sleep(25)
+#     print('done')
+#     bot.quit()
+
+
+# if __name__ == "__main__":
+#     RunScript()
