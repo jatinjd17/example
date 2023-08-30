d4bcc670955cffa15fb3c242704fb232f2dea6d3
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-08-30 13:51:15 +0530

    Update hairstylernew.py

--- hairstylernew.py
+++ hairstylernew.py
@@ -44,7 +44,7 @@ options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appl
 # options.add_argument('--window-size=1920,1080')
 options.add_argument("--headless")
 time.sleep(2)
-bot = webdriver.Chrome(options=option)
+bot = webdriver.Chrome(options=options)
 
 def RunScript():
     # bot = webdriver.Chrome(options=option)
