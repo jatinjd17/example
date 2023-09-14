cd4cdba20922cd9d234c56aa60773e1edfe73361
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-09-14 16:21:40 +0530

    Update languagetoolautonolink.py

--- languagetoolautonolink.py
+++ languagetoolautonolink.py
@@ -39,53 +39,10 @@ def RunScript():
     bot.get("https://google.com/")
     bot.get("https://themeisle.com/blog/best-grammar-checker-free/")
     time.sleep(5)
-    bot.get("https://languagetool.org/")
-    #bot.get("https://www.shareasale.com/r.cfm?b=2285581&u=3667753&m=138470")
-    time.sleep(5)
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="addon-link"]/a'))).click()
-    time.sleep(5)
-    bot.switch_to.window(bot.window_handles[1])
-    #bot.get("https://chrome.google.com/webstore/detail/grammar-checker-paraphras/oldceeleldhonbafppcapldpdifcinji?utm_source=lt-homepage")
-    #time.sleep(60)
-    #'Add to Chrome' webstore-test-button-label
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(),'Add to Chrome')]"))).click()
-    #WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div/div/div[2]/div[2]/div/div/div/div'))).click()
-
-    time.sleep(5)
-
-    hotkey('left', 'enter')
-    # time.sleep(4)
-
-
-    time.sleep(15)
-    bot.switch_to.window(bot.window_handles[2])
-
-    time.sleep(2)
-
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Okay, got it')]"))).click()
-    time.sleep(2)
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Start using LanguageTool')]"))).click()
-    time.sleep(2)
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Continue')]"))).click()
-    time.sleep(3)
-    get_url = bot.current_url
-    print("The current url is33333:"+str(get_url))
-
-
-    time.sleep(8)
-    WebDriverWait(bot,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[2]/div/textarea"))).send_keys('Show me the best Food')
-
-
-    #/html/body/div[3]/div[2]/div/textarea
-
-
-
-
-    #html = bot.find_element(By.XPATH, '/')
-    #html.send_keys(Keys.TAB)
-
-    #class="Je-qe-zd-Ge hg S-Zb-U Pj"
-    time.sleep(5)
+    # bot.get("https://languagetool.org/")
+    bot.get("https://www.shareasale.com/r.cfm?b=2285581&u=3667753&m=138470")
+    time.sleep(25)
+    
     bot.quit()
 
 if __name__ == "__main__":
